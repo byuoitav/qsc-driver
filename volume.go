f5eb79ca61f58ed75f2311728269273955413678
Andrew Werner <klinc9@gmail.com>
2020-06-23 09:51:46 -0600

    updating driver to match new api functions

--- volume.go
+++ volume.go
@@ -9,25 +9,17 @@ import (
 	"math"
 	"strconv"
 
-	"github.com/byuoitav/common/status"
 	"github.com/fatih/color"
 )
 
-func (d *DSP) Mute(ctx context.Context, name string) (status.Mute, error) {
-	return d.setMuteStatus(ctx, name, true)
-}
+func (d *DSP) SetMute(ctx context.Context, block string, mute bool) (bool, error) {
 
-func (d *DSP) UnMute(ctx context.Context, name string) (status.Mute, error) {
-	return d.setMuteStatus(ctx, name, false)
-}
-
-func (d *DSP) setMuteStatus(ctx context.Context, name string, value bool) (status.Mute, error) {
 	//we generate our set status request, then we ship it out
 
-	name = name + "Mute"
+	block = block + "Mute"
 	req := d.GetGenericSetStatusRequest(ctx)
-	req.Params.Name = name
-	if value {
+	req.Params.Name = block
+	if mute {
 		req.Params.Value = 1
 	} else {
 		req.Params.Value = 0
@@ -36,7 +28,7 @@ func (d *DSP) setMuteStatus(ctx context.Context, name string, value bool) (statu
 	resp, err := d.SendCommand(ctx, req)
 	if err != nil {
 		log.Printf(color.HiRedString("Error: %v", err.Error()))
-		return status.Mute{}, err
+		return false, err
 	}
 
 	//we need to unmarshal our response, parse it for the value we care about, then role with it from there
@@ -44,45 +36,46 @@ func (d *DSP) setMuteStatus(ctx context.Context, name string, value bool) (statu
 	err = json.Unmarshal(resp, &val)
 	if err != nil {
 		log.Printf(color.HiRedString("Error: %v", err.Error()))
-		return status.Mute{}, err
+		return false, err
 	}
 
 	//otherwise we check to see what the value is set to
-	if val.Result.Name != name {
-		errmsg := fmt.Sprintf("Invalid response, the name recieved does not match the name sent %v/%v", name, val.Result.Name)
+	if val.Result.Name != block {
+		errmsg := fmt.Sprintf("Invalid response, the name recieved does not match the name sent %v/%v", block, val.Result.Name)
 		log.Printf(color.HiRedString(errmsg))
-		return status.Mute{}, errors.New(errmsg)
+		return false, errors.New(errmsg)
 	}
 
 	if val.Result.Value == 1.0 {
-		return status.Mute{Muted: true}, nil
+		return true, nil
 	}
 	if val.Result.Value == 0.0 {
-		return status.Mute{Muted: false}, nil
+		return false, nil
 	}
 	errmsg := fmt.Sprintf("[QSC-Communication] Invalid response received: %v", val.Result)
 	log.Printf(color.HiRedString(errmsg))
-	return status.Mute{}, errors.New(errmsg)
+	return false, errors.New(errmsg)
 }
 
-func (d *DSP) SetVolume(ctx context.Context, name string, level int) (status.Volume, error) {
-	name = name + "Gain"
-	log.Printf("got: %v", level)
-	req := d.GetGenericSetStatusRequest(ctx)
-	req.Params.Name = name
+func (d *DSP) SetVolume(ctx context.Context, block string, volume int) error {
 
-	if level == 0 {
+	block = block + "Gain"
+	log.Printf("got: %v", volume)
+	req := d.GetGenericSetStatusRequest(ctx)
+	req.Params.Name = block
+
+	if volume == 0 {
 		req.Params.Value = -100
 	} else {
 		//do the logrithmic magic
-		req.Params.Value = d.VolToDb(ctx, level)
+		req.Params.Value = d.VolToDb(ctx, volume)
 	}
 	log.Printf("sending: %v", req.Params.Value)
 
 	resp, err := d.SendCommand(ctx, req)
 	if err != nil {
 		log.Printf(color.HiRedString("Error: %v", err.Error()))
-		return status.Volume{}, err
+		return err
 	}
 
 	//we need to unmarshal our response, parse it for the value we care about, then role with it from there
@@ -90,15 +83,15 @@ func (d *DSP) SetVolume(ctx context.Context, name string, level int) (status.Vol
 	err = json.Unmarshal(resp, &val)
 	if err != nil {
 		log.Printf(color.HiRedString("Error: %v", err.Error()))
-		return status.Volume{}, err
+		return err
 	}
-	if val.Result.Name != name {
-		errmsg := fmt.Sprintf("Invalid response, the name recieved does not match the name sent %v/%v", name, val.Result.Name)
+	if val.Result.Name != block {
+		errmsg := fmt.Sprintf("Invalid response, the name recieved does not match the name sent %v/%v", block, val.Result.Name)
 		log.Printf(color.HiRedString(errmsg))
-		return status.Volume{}, errors.New(errmsg)
+		return errors.New(errmsg)
 	}
 
-	return status.Volume{d.DbToVolumeLevel(ctx, val.Result.Value)}, nil
+	return nil
 }
 
 func (d *DSP) DbToVolumeLevel(ctx context.Context, level float64) int {
@@ -109,48 +102,74 @@ func (d *DSP) VolToDb(ctx context.Context, level int) float64 {
 	return math.Log10(float64(level)/100) * 20
 }
 
-func (d *DSP) GetVolume(ctx context.Context, name string) (status.Volume, error) {
+func (d *DSP) GetVolumes(ctx context.Context, blocks []string) (map[string]int, error) {
+	toReturn := make(map[string]int)
 
-	name = name + "Gain"
-	resp, err := d.GetControlStatus(ctx, name)
-	if err != nil {
-		log.Printf(color.HiRedString("There was an error: %v", err.Error()))
-		return status.Volume{}, err
-	}
-
-	log.Printf(color.HiBlueString("[QSC-Communication] Response received: %+v", resp))
-
-	//get the volume out of the dsp and run it through our equation to reverse it
-	for _, res := range resp.Result {
-		if res.Name == name {
-			return status.Volume{d.DbToVolumeLevel(ctx, res.Value)}, nil
+	for i, block := range blocks {
+		block = block + "Gain"
+		resp, err := d.GetControlStatus(ctx, block)
+		if err != nil {
+			log.Printf(color.HiRedString("There was an error: %v", err.Error()))
+			return toReturn, err
 		}
+
+		log.Printf(color.HiBlueString("[QSC-Communication] Response received: %+v", resp))
+
+		//get the volume out of the dsp and run it through our equation to reverse it
+		found := false
+		for _, res := range resp.Result {
+			if res.Name == block {
+				toReturn[blocks[i]] = d.DbToVolumeLevel(ctx, res.Value)
+				found = true
+				break
+			}
+		}
+		if found {
+			continue
+		}
+
+		return toReturn, errors.New("[QSC-Communication] No value returned with the name matching the requested state")
 	}
 
-	return status.Volume{}, errors.New("[QSC-Communication] No value returned with the name matching the requested state")
+	return toReturn, nil
 }
-func (d *DSP) GetMute(ctx context.Context, name string) (status.Mute, error) {
-	name = name + "Mute"
-	resp, err := d.GetControlStatus(ctx, name)
-	if err != nil {
-		log.Printf(color.HiRedString("There was an error: %v", err.Error()))
-		return status.Mute{}, err
-	}
 
-	//get the volume out of the dsp and run it through our equation to reverse it
-	for _, res := range resp.Result {
-		if res.Name == name {
-			if res.Value == 1.0 {
-				return status.Mute{Muted: true}, nil
-			}
-			if res.Value == 0.0 {
-				return status.Mute{Muted: false}, nil
+func (d *DSP) GetMutes(ctx context.Context, blocks []string) (map[string]bool, error) {
+	toReturn := make(map[string]bool)
+
+	for i, block := range blocks {
+		block = block + "Mute"
+		resp, err := d.GetControlStatus(ctx, block)
+		if err != nil {
+			log.Printf(color.HiRedString("There was an error: %v", err.Error()))
+			return toReturn, err
+		}
+
+		//get the volume out of the dsp and run it through our equation to reverse it
+		found := false
+		for _, res := range resp.Result {
+			if res.Name == block {
+				if res.Value == 1.0 {
+					toReturn[blocks[i]] = true
+					found = true
+				}
+				if res.Value == 0.0 {
+					toReturn[blocks[i]] = false
+					found = true
+				}
+				break
 			}
 		}
+		if found {
+			continue
+		}
+
+		errmsg := "[QSC-Communication] No value returned with the name matching the requested state"
+		log.Printf(color.HiRedString(errmsg))
+		return toReturn, errors.New(errmsg)
 	}
-	errmsg := "[QSC-Communication] No value returned with the name matching the requested state"
-	log.Printf(color.HiRedString(errmsg))
-	return status.Mute{}, errors.New(errmsg)
+
+	return toReturn, nil
 }
 
 func (d *DSP) GetControlStatus(ctx context.Context, name string) (QSCGetStatusResponse, error) {
